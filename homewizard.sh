b5bd84cfbd077b891e3591606889e530a00dfd80
Martin Gafner <gafner@puzzle.ch>
2016-12-08 22:15:18 +0100

    add licence text

--- homewizard.sh
+++ homewizard.sh
@@ -1,3 +1,18 @@
+# This file is part of homewizard-web-remote (hwwr)                                                    
+# https://github.com/mgafner/homewizard-web-remote                                            
+#                                                                                
+# hwwr is free software: you can redistribute it and/or modify                 
+# it under the terms of the GNU General Public License as published by            
+# the Free Software Foundation, either version 3 of the License, or               
+# (at your option) any later version.                                             
+#                                                                                
+# hwwr is distributed in the hope that it will be useful,                      
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                  
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   
+# GNU General Public License for more details.                                    
+#                                                                                
+# You should have received a copy of the GNU General Public License               
+# along with hwwr.  If not, see <http://www.gnu.org/licenses/>.
 
 HOMEWIZARD_IP="192.168.1.2"
 HOMEWIZARD_PW="yourpass"
